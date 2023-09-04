8fd8553de1bc45a2dc944a78537a9e9f328eac52
ishii07m <143371913+ishii07m@users.noreply.github.com>
2023-09-04 16:11:49 +0900

    Update kadai_008.py
    順序入れ替え

--- kadai_008/kadai_008.py
+++ kadai_008/kadai_008.py
@@ -1,11 +1,11 @@
 import random
 var = random.randint(1, 10)
 print(var)
-if var %3 == 0:
-  print("Fizz")
-elif var %5 == 0:
-  print("Bazz")
-elif var %3 == 0 and var %5 == 0:
+if var %3 == 0 and var %5 == 0:
   print("FizzBuzz")
+elif var %3 == 0:
+  print("Fizz")
+elif var %5 ==0:
+  print("Buzz")
 else:
-  print("and条件が成り立ちませんでした")
+  print(var)
