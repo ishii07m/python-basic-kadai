77e5b780caafcffe9a310eaef8c2e1815bfc6fd5
ishii07m <143371913+ishii07m@users.noreply.github.com>
2023-09-05 12:38:26 +0900

    Update kadai_013.py
    戻り値を返す形に変更

--- kadai_013/kadai_013.py
+++ kadai_013/kadai_013.py
@@ -1,4 +1,7 @@
-def calculate_total(price):
-  total = price * 1.1
-  print(f"{total}円")
-calculate_total(1000)
+def calculate_total_price(price, tax_rate):
+    total_price = price * 1.1
+    return total_price
+price = 1000 
+tax_rate = 1.1
+total_price = calculate_total_price(price, tax_rate)
+print(f"{total_price}円")
